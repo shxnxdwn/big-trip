8e0d3e0f83e623df192deb624f3a023ea68d4d3a
Gleb Kornilov <esplete@gmail.com>
2025-04-01 07:57:01 +0300

    feat: code refactor

--- src/presenter/event-list-presenter.js
+++ src/presenter/event-list-presenter.js
@@ -2,15 +2,15 @@ import AddEventView from '../view/add-event-view';
 import EditEventView from '../view/edit-event-view';
 import SortView from '../view/sort-view';
 import TripEventView from '../view/trip-event-view';
-import TripListView from '../view/trip-list-view';
+import EventListView from '../view/event-list-view';
 import {render} from '../framework/render';
 
 
 export default class EventListPresenter {
   #container = null;
   #eventListModel = null;
-  #eventList = null;
-  #tripListElement = new TripListView();
+  #eventList = [];
+  #eventListElement = new EventListView();
 
   constructor({container, eventListModel}) {
     this.#container = container;
@@ -21,12 +21,16 @@ export default class EventListPresenter {
     this.#eventList = [...this.#eventListModel.eventList];
 
     render(new SortView(), this.#container);
-    render(this.#tripListElement, this.#container);
-    render(new AddEventView({tripEvent: this.#eventList[0]}), this.#tripListElement.element);
-    render(new EditEventView({tripEvent: this.#eventList[1]}), this.#tripListElement.element);
+    render(this.#eventListElement, this.#container);
+    render(new AddEventView({tripEvent: this.#eventList[0]}), this.#eventListElement.element);
+    render(new EditEventView({tripEvent: this.#eventList[1]}), this.#eventListElement.element);
 
     for (let i = 2; i < this.#eventList.length; i++) {
-      render(new TripEventView({tripEvent: this.#eventList[i]}), this.#tripListElement.element);
+      render(new TripEventView({tripEvent: this.#eventList[i]}), this.#eventListElement.element);
     }
+  }
+
+  #renderEvent() {
+
   }
 }
--- src/view/event-list-view.js
+++ src/view/event-list-view.js
@@ -4,7 +4,7 @@ import AbstractView from '../framework/view/abstract-view';
 const createTripListTemplate = () => '<ul class="trip-events__list"></ul>';
 
 
-export default class TripListView extends AbstractView {
+export default class EventListView extends AbstractView {
   get template() {
     return createTripListTemplate();
   }
